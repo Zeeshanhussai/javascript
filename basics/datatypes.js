eac3868976ed66c5dc395954672d73602a77c955
z <96389098+Zeeshanhussai@users.noreply.github.com>
2023-09-08 02:14:59 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,32 @@
+"user strict"; // treat all js code as newer version.
+
+// alert (3+3) // we are using node.js, not browser engine so we will see that later
+
+console.log(3+ // see it's okay to do it, but it hampers code readability
+    3)
+
+let name = "zeeshan" // string data type 
+let age = 24        // number 
+let isLoggedin = true   // boolean 
+
+
+
+// number => 2 to the power 53
+// bigint 
+//string => " abcd "
+//boolean => true/false
+// null => standalone value 
+// indefined => 
+
+// symbol => this is to find uniqueness, mostly used in react components
+
+// object
+
+console.log(typeof age) // showed me number 
+console.log(typeof name) // showed me string
+
+// now lets see this
+
+console.log(typeof null) // it showed me object, becaues it is an object
+
+console.log(typeof undefined) // showed me undefined 
