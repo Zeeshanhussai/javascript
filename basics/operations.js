4527ebf681021f8f2f803dffc19cfe4baf89903c
z <96389098+Zeeshanhussai@users.noreply.github.com>
2023-09-09 02:58:50 +0000

    operations in js

---
+++ basics/operations.js
@@ -0,0 +1,24 @@
+//********************************* OPERATIONS**************************//
+
+// negative value
+
+let number = 3;
+let negnumber = -number;
+console.log(negnumber) // yup it returned me -3
+
+console.log(2+2); // ADD
+console.log(2-2); // SUBSTRACT
+console.log(2*2); //MULTIPLY
+console.log(2**3); //POWER  
+console.log(2/3); // DIVIDE
+console.log(2%3)// REMAINDER / MODULUS OPERATOR
+
+
+// CONCATINATION OF A STRING
+ 
+let str1 = "hello "
+let str2 = " zeeshan"
+
+let str3 = str1 + str2;
+
+console.log(str3)
